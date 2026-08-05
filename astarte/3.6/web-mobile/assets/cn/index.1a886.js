4f1d799a681660c29be6ea040d2b1e4fe8fbebbb
sgkj6688 <sgkj6688@gmail.com>
2026-08-05 18:52:23 +0800

    1

--- astarte/3.6/web-mobile/assets/cn/index.1a886.js
+++ astarte/3.6/web-mobile/assets/cn/index.1a886.js
@@ -1,20 +1,25 @@
-System.register("chunks:///cn.js",[],(function(){"use strict";return{execute:function(){}}}));
+System.register("chunks:///cn.js", [], (function() {
+    "use strict";
+    return {
+        execute: function() {}
+    }
+}));
 
 (function(r) {
-  r('virtual:///prerequisite-imports/cn', 'chunks:///cn.js'); 
+    r('virtual:///prerequisite-imports/cn', 'chunks:///cn.js');
 })(function(mid, cid) {
-    System.register(mid, [cid], function (_export, _context) {
-    return {
-        setters: [function(_m) {
-            var _exportObj = {};
+    System.register(mid, [cid], function(_export, _context) {
+        return {
+            setters: [function(_m) {
+                var _exportObj = {};
 
-            for (var _key in _m) {
-              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
-            }
-      
-            _export(_exportObj);
-        }],
-        execute: function () { }
-    };
+                for (var _key in _m) {
+                    if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+                }
+
+                _export(_exportObj);
+            }],
+            execute: function() {}
+        };
     });
 });
